36b0ff865a210971b0d63b5193bfb44f9d4f0f36
RoliSoft <rolisoft@gmail.com>
2016-02-11 04:38:15 +0200

    Added parameters to convert.sh

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ -f cpe-aliases ]]; then
+if [[ -z $1 || $1 == "cpealt" ]] && [[ -f cpe-aliases ]]; then
 	rm -f cpe-aliases.dat cpe-aliases.dat.gz
 	go run cpealt2hs.go cpe-aliases cpe-aliases.dat
 	gzip -9 cpe-aliases.dat
@@ -14,13 +14,13 @@ if [[ -f nmap-service-probes ]]; then
 	:
 fi
 
-if [[ -f cpe-dict.xml ]]; then
+if [[ -z $1 || $1 == "cpe" ]] && [[ -f cpe-dict.xml ]]; then
 	rm -f cpe-list.dat cpe-list.dat.gz
 	go run cpe2hs.go cpe-dict.xml cpe-list.dat
 	gzip -9 cpe-list.dat
 fi
 
-if [[ -f cve-items.xml ]]; then
+if [[ -z $1 || $1 == "cve" ]] && [[ -f cve-items.xml ]]; then
 	rm -f cve-list.dat cve-list.dat.gz
 	go run cve2hs.go cve-items.xml cve-list.dat
 	gzip -9 cve-list.dat
